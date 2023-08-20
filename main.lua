d1b0a179baa90caf945d8108202e83d7d3b550fe
vertfire <60331737+vertfire@users.noreply.github.com>
2023-08-20 12:59:54 -0400

    Remove The Old Inheritance File

--- main.lua
+++ main.lua
@@ -1,13 +1,42 @@
 
-BaseClass = {}
-BaseClass.__type = "BaseClass"
-BaseClass.__index = BaseClass;
-setmetatable(BaseClass,{__index=BaseClass})
-function BaseClass.new ()
-    local obj = {}
+require("Class")
+
+BaseClass = class("BaseClass")
+
+function BaseClass.new (number, otherNumber)
+    local obj = {
+        a = number,
+        b = otherNumber; 
+    }
     setmetatable(obj,BaseClass);
     return obj
 end
 
+function BaseClass:add ()
+    return self.a + self.b
+end
 
 
+
+local b = BaseClass.new(1,3)
+print(b:add()); 
+
+
+SubClass = class()
+
+function SubClass.new (a,b)
+    local obj = {
+        a = a,
+        b = b; 
+    }
+    setmetatable(obj,SubClass);
+    return obj
+end
+
+function SubClass:subtract ()
+    return self.a - self.b; 
+end
+
+
+local c = SubClass.new(1,3)
+print(c:subtract()); 
